e1bbab0f2e14eae6fe4c580e97b9dfe6f286e56f
李智霖 <lizhilin@wowbook.local>
2026-09-01 22:17:24 +0800

    学习Python基础输入输出

---
+++ 4.py
@@ -0,0 +1,13 @@
+# 输入输出字符
+print('The quick brown fox','jumps over','the lazy dog') 
+
+# 可以让打印变得更漂亮
+print('100 + 200 =',100 + 200) 
+
+# 让用户输入内容,以及输出你好xxx，注意此处name是字符串变量
+name = input('请输入你的名字：') 
+print('hello',name)
+
+# 第四章节练习
+X = 1024 * 768 
+print(X)
